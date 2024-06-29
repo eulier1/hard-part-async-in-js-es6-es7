1bcc7fee1c3caedc9fc5a9cd16d47e763ba643b2
Eulier Gonzalez <eulier.gonzalez@bisonok.com>
2024-06-29 18:33:45 -0300

    add async-await template

--- async-await.js
+++ async-await.js
@@ -1,12 +1,4 @@
-function doWhenDataReceived(data) {
-    returnNextElement.next(data)
+async function createFlow() {
+    console.log("Me first")
+    // Todo
 }
-
-function* createFlow() {
-    const data = yield fetch("https://dog.ceo/api/breeds/image/random")
-    console.log(data)
-}
-
-const returnNextElement = createFlow()
-const futureData = returnNextElement.next().value
-futureData.then(doWhenDataReceived)
